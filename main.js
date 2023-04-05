bc27e0318479315d97a321e464e5302adbb3f27e
Apix <apix0n@outlook.com>
2023-04-05 18:21:08 +0000

    🛠️ | Added invite.html and updated character guide - Added invite.html in French and English - Revamped character guide and styles to match with invite.html - Added new characters - Removed unnecessary JS

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 console.log('%cArmus', 'color: #ff0000; font-size: 55px')
 console.log("Typing here may be dangerous: be careful and NEVER paste scripts from strangers.")
 
-// https://css-tricks.com/snippets/javascript/get-url-variables/
 function getQueryVariable(variable) {
     var query = window.location.search.substring(1);
     var vars = query.split("&");
@@ -9,23 +8,6 @@ function getQueryVariable(variable) {
         var pair = vars[i].split("=");
         if (pair[0] == variable) { return pair[1]; }
     }
-    return (false);
-}
-
-// Hides .lp-selector-mp when launched as a PWA.
-var launchParameter = getQueryVariable('launch');
-if (launchParameter == "pwa") {
-    var lpSelector = document.getElementsByClassName("lp-selector-mp")[0];
-    lpSelector.style.display = "none";
-    console.log("Website was launched as a PWA.")
-}
-
-// Sets href in Create a character if user came from Turkish
-var langParameter = getQueryVariable('lang');
-if (langParameter == "tr") {
-    var newLangUrl = "../tr/#character"
-    document.getElementById("backbutton").href = newLangUrl;
-    console.log("User came from Turkish webpage.")
 }
 
 // Rickroll functions
@@ -34,12 +16,56 @@ function rick() {
     border.style.display = "block";
     var video = document.getElementById("rickroll");
     video.currentTime = 0;
-    video.play()
+    video.play();
 }
 
 function closerick() {
     var border = document.getElementsByClassName("rick")[0];
     border.style.display = "none";
     var video = document.getElementById("rickroll");
-    video.pause()
+    video.pause();
+}
+
+// Function to support invite sharing
+async function shareInvite() {
+    var inviterNameElement = document.getElementById("invitername-input");
+    var inviterName = inviterNameElement.value;
+    var inviteLink = document.URL + "invite.html?inviter=" + inviterName
+    const shareData = { title: "You got invited to join Armus!", text: "Join Armus!", url: inviteLink, };
+    try {
+        await navigator.share(shareData);
+    } catch (err) {
+        console.log("Can't use the Web Share API. Fallback to clipboard.");
+        await navigator.clipboard.writeText(inviteLink);
+        alert("Copied to clipboard!")
+    }
+}
+
+// Function to support invite sharing (only english version)
+async function shareInviteEnglish() {
+    var inviterNameElement = document.getElementById("invitername-input");
+    var inviterName = inviterNameElement.value;
+    var currentUrl = window.location.href;
+    var inviteLink = currentUrl.split('/').slice(0, 3).join('/') + "/en/invite.html?inviter=" + inviterName;
+    const shareData = { title: "You got invited to join Armus!", text: "Join Armus!", url: inviteLink };
+    try {
+        await navigator.share(shareData);
+    } catch (err) {
+        console.log("Can't use the Web Share API. Fallback to clipboard.");
+        await navigator.clipboard.writeText(inviteLink);
+        alert("Copied to clipboard!")
+    }
+}
+
+// Changes inviter name on invite.html
+function changeInviterName() {
+    var inviterParameter = getQueryVariable('inviter');
+    var inviterText = document.getElementsByClassName("inviter-name")[0];
+    if (inviterParameter == undefined) {
+        console.log("No custom inviter name... skipping");
+    }
+    else {
+        console.log("Changing inviter name...");
+        inviterText.innerText = decodeURI(inviterParameter);
+    }
 }
